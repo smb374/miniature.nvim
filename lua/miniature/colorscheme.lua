9a2db3383a63a83c94c2bfcf68562836cdd8060d
Po-Yeh Chen <smb374.cs08@nctu.edu.tw>
2022-03-24 00:07:33 +0800

    Change how theme is assigned.

--- lua/miniature/colorscheme.lua
+++ lua/miniature/colorscheme.lua
@@ -3,33 +3,34 @@ if not present then
 	return
 end
 
-local theme = {
-	base00 = "#1b1c27",
-	base01 = "#303146",
-	base02 = "#444764",
-	base03 = "#595c82",
-	base04 = "#70749e",
-	base05 = "#8f92b3",
-	base06 = "#adafc7",
-	base07 = "#cbcddc",
-	base08 = "#f7768e",
-	base09 = "#ff9e64",
-	base0A = "#e0af68",
-	base0B = "#9ece6a",
-	base0C = "#2ac3de",
-	base0D = "#7aa2f7",
-	base0E = "#9d7cd8",
-	base0F = "#f55673",
-}
+-- You should define your own base16 colorscheme here
+-- or load a base16 theme externally.
+-- miniature.colorscheme will work as long as the theme
+-- has the same format as below.
+local theme = {}
+
+theme.base00 = "#1b1c27"
+theme.base01 = "#303146"
+theme.base02 = "#444764"
+theme.base03 = "#595c82"
+theme.base04 = "#70749e"
+theme.base05 = "#8f92b3"
+theme.base06 = "#adafc7"
+theme.base07 = "#cbcddc"
+theme.base08 = "#f7768e"
+theme.base09 = "#ff9e64"
+theme.base0A = "#e0af68"
+theme.base0B = "#9ece6a"
+theme.base0C = "#2ac3de"
+theme.base0D = "#7aa2f7"
+theme.base0E = "#9d7cd8"
+theme.base0F = "#f55673"
 
 base16.setup({
 	palette = theme,
 })
 
 local highlight = function(group, args)
-	-- NOTE: using `string.format` instead of gradually growing string with `..`
-	-- is faster. Crude estimate for this particular case: whole colorscheme
-	-- loading decreased from ~3.6ms to ~3.0ms, i.e. by about 20%.
 	local command = string.format(
 		[[highlight %s guifg=%s guibg=%s gui=%s guisp=%s]],
 		group,
@@ -37,7 +38,7 @@ local highlight = function(group, args)
 		args.bg or "NONE",
 		args.attr or "NONE",
 		args.sp or "NONE"
-	)
+  )
 	vim.cmd(command)
 end
 
@@ -48,11 +49,12 @@ vim.cmd([[
   hi Comment  gui=italic
 ]])
 
+-- nvim-cmp highlights
 highlight("CmpDocumentation", { fg = theme.base05, bg = theme.base00 })
 highlight("CmpDocumentationBorder", { fg = theme.base0D, bg = theme.base00 })
 
 highlight("CmpItemAbbr", { fg = theme.base05, bg = nil })
-highlight("CmpItemAbbrDeprecated", { fg = theme.base03, bg = nil, style = "strikethrough" })
+highlight("CmpItemAbbrDeprecated", { fg = theme.base03, bg = nil, attr = "strikethrough" })
 highlight("CmpItemAbbrMatch", { fg = theme.base0D, bg = nil })
 highlight("CmpItemAbbrMatchFuzzy", { fg = theme.base0D, bg = nil })
 
