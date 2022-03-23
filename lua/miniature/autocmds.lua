76e8898e5fa1134adfd86ba0526213c3b2f242b3
Po-Yeh Chen <smb374.cs08@nctu.edu.tw>
2022-03-23 15:23:21 +0800

    Remove colors/ to reduce code.

--- lua/miniature/autocmds.lua
+++ lua/miniature/autocmds.lua
@@ -54,11 +54,3 @@ vim.cmd([[
     autocmd BufUnload <buffer> set showtabline=2
   augroup end
 ]])
-
--- Autoformat
-vim.cmd([[
-  augroup _lsp
-    autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting()
-  augroup end
-]])
--- lua/miniature/colorscheme.lua
+++ lua/miniature/colorscheme.lua
@@ -1,40 +1,57 @@
 local present, base16 = pcall(require, "mini.base16")
 if not present then
-  return
+	return
 end
 
 local theme_name = vim.g.miniature_color or "default-dark"
 
 local theme_present, theme = pcall(require, string.format("colors.%s", theme_name))
 if not theme_present then
-  theme = require("colors.default-dark")
+	theme = require("colors.default-dark")
 end
 
 base16.setup({
-  palette = theme
+	palette = {
+		base00 = "#1b1c27",
+		base01 = "#303146",
+		base02 = "#444764",
+		base03 = "#595c82",
+		base04 = "#70749e",
+		base05 = "#8f92b3",
+		base06 = "#adafc7",
+		base07 = "#cbcddc",
+		base08 = "#f7768e",
+		base09 = "#ff9e64",
+		base0A = "#e0af68",
+		base0B = "#9ece6a",
+		base0C = "#2ac3de",
+		base0D = "#7aa2f7",
+		base0E = "#9d7cd8",
+		base0F = "#f55673",
+	},
 })
 
-local highlight = function (group, args)
-  -- NOTE: using `string.format` instead of gradually growing string with `..`
-  -- is faster. Crude estimate for this particular case: whole colorscheme
-  -- loading decreased from ~3.6ms to ~3.0ms, i.e. by about 20%.
-  local command = string.format(
-    [[highlight %s guifg=%s guibg=%s gui=%s guisp=%s]],
-    group,
-    args.fg or 'NONE',
-    args.bg or 'NONE',
-    args.attr or 'NONE',
-    args.sp or 'NONE'
-  )
-  vim.cmd(command)
+local highlight = function(group, args)
+	-- NOTE: using `string.format` instead of gradually growing string with `..`
+	-- is faster. Crude estimate for this particular case: whole colorscheme
+	-- loading decreased from ~3.6ms to ~3.0ms, i.e. by about 20%.
+	local command = string.format(
+		[[highlight %s guifg=%s guibg=%s gui=%s guisp=%s]],
+		group,
+		args.fg or "NONE",
+		args.bg or "NONE",
+		args.attr or "NONE",
+		args.sp or "NONE"
+	)
+	vim.cmd(command)
 end
 
 -- Italics
-vim.cmd [[
+vim.cmd([[
   hi Function gui=italic
   hi Keyword  gui=italic
   hi Comment  gui=italic
-]]
+]])
 
 --[[
 " gray
@@ -88,10 +105,9 @@ highlight("CmpItemKindModule", { fg = theme.base0A, bg = nil })
 
 highlight("CmpItemKindProperty", { fg = theme.base0B, bg = nil })
 highlight("CmpItemKindField", { fg = theme.base0B, bg = nil })
-highlight("CmpItemKindTypeParameter" , { fg = theme.base0B, bg = nil })
+highlight("CmpItemKindTypeParameter", { fg = theme.base0B, bg = nil })
 highlight("CmpItemKindEnumMember", { fg = theme.base0B, bg = nil })
 highlight("CmpItemKindOperator", { fg = theme.base0B, bg = nil })
 highlight("CmpItemKindSnippet", { fg = theme.base06, bg = nil })
 
 vim.g.colors_name = "miniature_color"
-
